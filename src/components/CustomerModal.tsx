64c2d5eec551010bb2129ae6d4a40498c1cc3f78
linh <linh.bui@nextlevel.vn>
2020-08-02 09:31:49 +0700

    FOOD-13 customer modal fix modal

--- src/components/CustomerModal.tsx
+++ src/components/CustomerModal.tsx
@@ -11,33 +11,31 @@ const CustomerModal = ({
   handleClose,
 }: CustomerModalProps): ReactElement => {
   return (
-    <>
-      <Modal show={show} centered>
-        <Modal.Body id="modal-body">
-          <h4 className="text-center">Wellcome back</h4>
-          <Form.Group>
-            <Form.Label className="font-weight-bold">Name:</Form.Label>
-            <p>{name}</p>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label className="font-weight-bold">Address:</Form.Label>
-            <p>{address}</p>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label className="font-weight-bold">Status:</Form.Label>
-            <p>{status}</p>
-          </Form.Group>
-          <div className="d-flex justify-content-center mt-3">
-            <Button variant="primary" className="c-black">
-              Scan Product
-            </Button>
-          </div>
-          <p className="c-gray d-flex justify-content-center mt-3">
-            Not you? <span onClick={handleClose}> Click here</span>
-          </p>
-        </Modal.Body>
-      </Modal>
-    </>
+    <Modal show={show} centered>
+      <Modal.Body>
+        <h4 className="text-center">Wellcome back</h4>
+        <Form.Group>
+          <Form.Label className="font-weight-bold">Name:</Form.Label>
+          <p>{name}</p>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label className="font-weight-bold">Address:</Form.Label>
+          <p>{address}</p>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label className="font-weight-bold">Status:</Form.Label>
+          <p>{status}</p>
+        </Form.Group>
+        <div className="d-flex justify-content-center mt-3">
+          <Button variant="primary" className="c-black">
+            Scan Product
+          </Button>
+        </div>
+        <p className="c-gray d-flex justify-content-center mt-3">
+          Not you? <span onClick={handleClose}> Click here</span>
+        </p>
+      </Modal.Body>
+    </Modal>
   )
 }
 export default CustomerModal
