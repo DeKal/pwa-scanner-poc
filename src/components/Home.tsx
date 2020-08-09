1dfb6d2444b90d7460b1a9cf388d058f2a79416c
Phat Ho <hhphat@apcs.vn>
2020-08-09 15:21:16 +0700

    FOOD-20: Move img to assets in src for better caching

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types'
 import 'components/Home.scss'
 import Button from 'react-bootstrap/Button'
 import { HomeProps } from 'types/props'
+import logo from 'assets/logo.jpg'
 
 const Home = ({ history, removeUserSession }: HomeProps): ReactElement => {
   return (
     <div className="home-container">
       <div>
-        <img src={'/images/logo.jpg'} className="logo mt-5 mb-5" alt="logo" />
+        <img src={logo} className="logo mt-5 mb-5" alt="logo" />
 
         <div className="mt-3 mb-3">
           <h5 className="mb-2">Welcome to</h5>
